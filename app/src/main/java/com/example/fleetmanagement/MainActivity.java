d1d6501c17d228519b16483999e259d6d14e3ffe
MD RAZON HOSSAIN <razon@MDs-MacBook-Pro.local>
2023-07-11 01:22:56 +1000

    Week_2 is done

--- app/src/main/java/com/example/fleetmanagement/MainActivity.java
+++ app/src/main/java/com/example/fleetmanagement/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.fleetmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Varibales
+    private TextView titleTextView;
+    private Button loginButton;
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Linking the views
+        setViewIds();
+
+        //Setting Click listeners
+        loginButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        signupButton.setOnClickListener(view -> {
+            Toast.makeText(this, "Signup button Clicked", Toast.LENGTH_LONG).show();
+        });
+
+    }
+
+    private void setViewIds() {
+        titleTextView = findViewById(R.id.title);
+        loginButton = findViewById(R.id.btnLogin);
+        signupButton = findViewById(R.id.btnSignup);
     }
 }

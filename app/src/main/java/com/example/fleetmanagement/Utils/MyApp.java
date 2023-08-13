1aa4265cb362727debec8ddaa0f48b7935cb0f46
razon30 <hossainmdrazon@gmail.com>
2023-08-14 00:26:01 +1000

    Week_5 in progress

---
+++ app/src/main/java/com/example/fleetmanagement/Utils/MyApp.java
@@ -0,0 +1,22 @@
+package com.example.fleetmanagement.Utils;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+
+public class MyApp extends Application {
+
+    public static final String PREF_NAME = "MyAppPrefs";
+    private static SharedPreferences preferences;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        preferences = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+    }
+
+    public static SharedPreferences getPreferences() {
+        return preferences;
+    }
+}
+
+

78fcf5fe2903ea442e88ba6fa5b7cf0d5269fc17
razon30 <hossainmdrazon@gmail.com>
2023-08-26 11:35:55 +1000

    vehicle retrieved in vehicledetials activity

--- app/src/main/java/com/example/fleetmanagement/VehicleDetailsActivity.java
+++ app/src/main/java/com/example/fleetmanagement/VehicleDetailsActivity.java
@@ -2,12 +2,19 @@ package com.example.fleetmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fleetmanagement.DB.VehicleDao;
+import com.example.fleetmanagement.Utils.MyApp;
 
 public class VehicleDetailsActivity extends AppCompatActivity {
 
     TextView tvVehicleName, tvVehicleType;
+    VehicleDao vehicleDao;
+    int vehicleId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,20 @@ public class VehicleDetailsActivity extends AppCompatActivity {
 
         tvVehicleName = findViewById(R.id.tvVehicleName);
         tvVehicleType = findViewById(R.id.tvVehicleType);
+
+        vehicleDao = MyApp.getAppDatabase().vehicleDao();
+
+        if (getIntent().getIntExtra("vehicleId", -1) != -1){
+            vehicleId = getIntent().getIntExtra("vehicleId", -1);
+
+            vehicleDao.getVehicleById(vehicleId).observe(this, vehicle -> {
+                tvVehicleName.setText("Vehicle Name: "+ vehicle.getName());
+                tvVehicleType.setText("Vehicle Type: "+ vehicle.getType());
+            });
+
+        }else {
+            Toast.makeText(this, "No Vehicle Id Found", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
